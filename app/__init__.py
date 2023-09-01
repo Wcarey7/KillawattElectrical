70b96740e8b01ea295a0b000cf59e585f7351412
Wcarey7 <wadecarey7@gmail.com>
2023-09-01 14:06:23 -0700

    Error handling. File logging

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,9 @@ from app.extensions import bootstrap
 from app.models.user import User
 from flask import Flask
 from config import Config
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 
 
 def create_app(config_class=Config):
@@ -37,6 +40,9 @@ def create_app(config_class=Config):
     from app.routes.home_route import bp as home_bp
     app.register_blueprint(home_bp)
 
+    from app.errors.handlers import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
     from app.routes.customer_route import bp as customer_bp
     app.register_blueprint(customer_bp, url_prefix='/customer')
 
@@ -45,5 +51,24 @@ def create_app(config_class=Config):
 
     from app.routes.user_profile_route import bp as user_profile_bp
     app.register_blueprint(user_profile_bp, url_prefix='/user')
-    
+
+
+    ###################################################
+    #### Error Logging
+    ###################################################
+    if not app.debug and not app.testing:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/Killawatt.log',
+                                            maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Killawatt startup')
+
+                    
     return app
