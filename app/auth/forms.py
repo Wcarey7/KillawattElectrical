46aa09f2c2bbb85d288acfd9ebc0b04f124d4652
Wcarey7 <wadecarey7@gmail.com>
2023-10-19 14:55:14 -0700

    Client side input validation User registration

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -17,20 +17,20 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])           # Email() validator removed, was raising an error during Unit Tests
     password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
     def validate_username(self, username):
         user = db.session.execute(db.select(User).filter_by(username=username.data)).scalar()
         if user is not None:
             flash('Username already in use. Please choose a different username.')
-            raise ValidationError('Please use a different username.')
+            raise ValidationError('Username already in use. Please choose a different username.')
 
     def validate_email(self, email):
         user = db.session.execute(db.select(User).filter_by(email=email.data)).scalar()
         if user is not None:
             flash('Email already registered. Please use a different email.')
-            raise ValidationError('Please use a different email address.')
+            raise ValidationError('Email already registered. Please use a different email.')
 
     def validate_password2(self, password2):
         if self.password.data != password2.data:
