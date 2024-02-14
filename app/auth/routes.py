a0f74ded614a8d00add8c2da8339b34e980c013d
Wcarey7 <wadecarey7@gmail.com>
2024-02-14 10:39:26 -0800

    User register route adjusted for new password hashing method

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -14,8 +14,10 @@ def register():
 
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
+        user = User(username=form.username.data,
+                    email=form.email.data,
+                    password=form.password.data,
+                    )
         db.session.add(user)
         db.session.commit()
         flash('You are now a registered user!')
